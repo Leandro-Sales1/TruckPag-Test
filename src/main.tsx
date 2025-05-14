caf4a3d36b662d08994d6272ec3c8b8dc7243a1f
Leandro Sales <lsl.2014@hotmail.com>
2025-05-13 21:49:22 -0300

    - Solving type error on Jest .toBeInTheDocument - Building OK.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './main.css'
-import App from './index.tsx'
-import { FilmsProvider } from './context/FilmsContext.tsx'
+import App from './index'
+import { FilmsProvider } from './context/FilmsContext'
 
 
 createRoot(document.getElementById('root')!).render(
